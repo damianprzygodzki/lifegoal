5bdeaea9a500f10dc75c1fc30c070ecb218da388
damian.przygodzki <damian.przygodzki@jazzy.pro>
2016-07-03 17:05:10 +0200

    Local storage support

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,11 +4,19 @@ import { createStore, applyMiddleware } from 'redux'
 import { routerMiddleware } from 'react-router-redux'
 
 export default function configureStore(history){
+
+    const persistedState = localStorage.getItem('lifeGoalStore') ? {goalStateHandler: JSON.parse(localStorage.getItem('lifeGoalStore'))} : {}
+
     const middleware = [thunk, routerMiddleware(history)]
     const store = createStore(
         rootReducer,
+        persistedState,
         applyMiddleware(...middleware)
     )
+
+    store.subscribe(()=>{
+        localStorage.setItem('lifeGoalStore', JSON.stringify(store.getState().goalStateHandler))
+    })
 
     if (module.hot) {
         module.hot.accept('../reducers', () => {
